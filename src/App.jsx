f5cf5b688ada6f4f0f0f9365aaaabd63e5b9dde2
Tomás Cárdenas <tomicardenas96@gmail.com>
2024-07-16 15:28:19 -0300

    Initialized an empty project on reactjs for frontend

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* <Header />
+      <Hero /> */}
+      <main>
+        <section className="header_container">Header</section>
+        <section className="presentation_container">Presentation</section>
+        <section className="footer_container">Footer</section>
+      </main>
+    </>
+  );
+}
+
+export default App;
